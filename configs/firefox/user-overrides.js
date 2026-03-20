428560fd88152d2b3a43220fd23b461340233612
jehan593 <jehanss@protonmail.com>
2026-03-20 07:38:49 +0530

    minor change

--- configs/firefox/user-overrides.js
+++ configs/firefox/user-overrides.js
@@ -1,5 +1,4 @@
 user_pref("browser.search.suggest.enabled", true);
-user_pref("browser.contentblocking.category", "");
 user_pref("privacy.globalprivacycontrol.enabled", false);
 user_pref("gfx.webrender.software", true);
 user_pref("browser.sessionstore.resume_from_crash", false);
